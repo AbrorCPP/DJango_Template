947e0123682e1d05b910422c2e3bda010e8fe605
abror <abrorboltayev4843@gmail.com>
2026-04-17 20:51:13 +0500

    add main app

---
+++ app_main/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(verbose_name="Kategoriya nomi", max_length=100)
+    
+
+    def __str___(self):
+        return self.name
